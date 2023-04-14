93043f4dba2bd3d2540fe36bac334f85507542f1
mullak99 <mullak99@mullak99.co.uk>
2023-04-14 01:11:32 +0100

    Better handle anonymous API calls

--- Obsidian.App/Client/ApiService.cs
+++ Obsidian.App/Client/ApiService.cs
@@ -20,43 +20,44 @@ namespace Obsidian.App.Client
 
 		public async Task<T?> GetAsync<T>(string url, bool allowAnonymous = false, int retryCount = 3, int delayMilliseconds = 100)
 		{
-			var accessTokenResult = await _accessTokenProvider.RequestAccessToken();
-			var retry = 0;
+			string fullUrl = $"{_endpoint}/api/{url.TrimStart('/')}";
+			int retry = 0;
 
 			while (retry < retryCount)
 			{
-				AccessToken? accessToken = null;
-				if (allowAnonymous || accessTokenResult.TryGetToken(out accessToken))
+				if (!allowAnonymous)
 				{
-					if (!allowAnonymous && accessToken != null)
-						_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Value);
+					AccessTokenResult? accessTokenResult = await _accessTokenProvider.RequestAccessToken();
+					if (!accessTokenResult.TryGetToken(out var accessToken))
+						throw new HttpRequestException("You need to be logged in!");
+					_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Value);
+				}
 
-					try
-					{
-						var request = new HttpRequestMessage(HttpMethod.Get, $"{_endpoint}/api/{url.TrimStart('/')}");
-						request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-						var response = await _httpClient.SendAsync(request);
+				try
+				{
+					var request = new HttpRequestMessage(HttpMethod.Get, fullUrl);
+					request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					var response = await _httpClient.SendAsync(request);
 
-						if (response.IsSuccessStatusCode)
-						{
-							var responseContent = await response.Content.ReadAsStringAsync();
-							return JsonConvert.DeserializeObject<T>(responseContent); // System.Net.Json doesn't work reliably, using Newtonsoft instead
-						}
-					}
-					catch (HttpRequestException ex)
+					if (response.IsSuccessStatusCode)
 					{
-						throw new HttpRequestException("Could not communicate with the API. Is the API online?", ex);
+						var responseContent = await response.Content.ReadAsStringAsync();
+						return JsonConvert.DeserializeObject<T>(responseContent); // System.Net.Json doesn't work reliably, using Newtonsoft instead
 					}
+				}
+				catch (HttpRequestException ex)
+				{
+					throw new HttpRequestException("Could not communicate with the API. Is the API online?", ex);
 				}
 
 				// If the access token is not available or the request fails, retry after a delay
 				await Task.Delay(delayMilliseconds);
 				retry++;
-				accessTokenResult = await _accessTokenProvider.RequestAccessToken();
 			}
 
 			// If the API request failed after retrying, return null or throw an exception
-			throw new HttpRequestException($"API request to {url} failed after {retryCount} retries.");
+			throw new HttpRequestException($"API request to {fullUrl} failed after {retryCount} retries.");
 		}
+
 	}
 }
